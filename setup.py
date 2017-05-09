6ee8ed69b4ac64197cd9162aa552d42f3a1503a7
Jeremy Bowers <jeremyjbowers@gmail.com>
2017-05-09 14:06:54 -0400

    Ah, the first typo. Cool.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author='Jeremy Bowers',
     author_email='jeremy.bowers@nytimes.com',
     url='https://github.com/newsdev/nyt-pyiap',
-    description='Python utility functions and Django/Flask middlewares for validating JWT tokens from Google\'s Identity-Aware Proxy,
+    description='Python utility functions and Django/Flask middlewares for validating JWT tokens from Google\'s Identity-Aware Proxy',
     long_description=read('README.rst'),
     packages=('pyiap'),
     entry_points={},
