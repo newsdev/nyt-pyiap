b68b2931587318bd73a8ffe3b2017227905d0ef1
Jeremy Bowers <jeremyjbowers@gmail.com>
2017-05-25 13:48:42 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ try:
 except ImportError:
     from distutils.core import setup
 
-install_reqs = parse_requirements('requirements.txt', session=PipSession())
-reqs = [str(ir.req) for ir in install_reqs]
-
 
 def read(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
@@ -27,7 +24,7 @@ setup(
     entry_points={},
     license="Apache License 2.0",
     keywords='google identity-aware proxy iap jwt',
-    install_requires=reqs,
+    install_requires=['pyjwt', 'requests'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
