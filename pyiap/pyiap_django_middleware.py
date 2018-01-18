48c7b2b493cc89149d2d19562e68b61a9e85bd05
rshorey <rshorey@gmail.com>
2018-01-18 11:19:43 -0500

    use the new deprecation middleware class to make middleware compatible to all djangos

--- pyiap/pyiap_django_middleware.py
+++ pyiap/pyiap_django_middleware.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
-
 from pyiap.utils import validate_iap_jwt
 
 
-class VerifyJWTMiddleware(object):
+#the below should hopefully make the middleware compatible with djangos 1.x and 2.x
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:
+    MiddlewareMixin = object
+
+class VerifyJWTMiddleware(MiddlewareMixin):
     def process_request(self, request):
         """
         Adds attributes to the Django request object so that we can access
