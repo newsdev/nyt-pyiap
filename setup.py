fd1a2a33f79bb8aaa78a20f8777afdd797517379
Jeremy Bowers <jeremyjbowers@gmail.com>
2018-01-24 15:02:23 -0500

    Sigh

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(filename):
 
 setup(
     name='nyt-pyiap',
-    version='0.1.1',
+    version='0.1.2',
     author='Jeremy Bowers',
     author_email='jeremy.bowers@nytimes.com',
     url='https://github.com/newsdev/nyt-pyiap',
